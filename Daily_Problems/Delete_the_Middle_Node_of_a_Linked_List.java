14f9b4aadb0b237f0f4a3e0d5d4e38e03660a294
jaishnaviinturi <jaishnaviinturi@gmail.com>
2026-06-15 23:16:32 +0530

    Added Daily Problem - 15/06/2026

---
+++ Daily_Problems/Delete_the_Middle_Node_of_a_Linked_List.java
@@ -0,0 +1,56 @@
+//Link: https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/description/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        ListNode slow=head,fast=head;
+        int c=0;
+        if(head==null || head.next ==null){
+            return null;
+        }
+        while(fast!=null && fast.next!=null){
+            c++;
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        fast=head;
+        for(int i=1;i<c;i++){
+            System.out.print(fast.val);
+            fast=fast.next;
+        }
+        if(fast!=null && fast.next!=null){
+        fast.next=fast.next.next;
+        }
+        fast=head;
+        return fast;       
+    }
+}
+
+/*
+Best Solution:
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        if(head.next==null){
+            return null;
+        }
+        ListNode fast=head;
+        ListNode slow=new ListNode(-1);
+        slow.next=head;
+        while(fast!=null && fast.next!=null ){
+            fast=fast.next.next;
+            slow=slow.next;
+        }
+        slow.next=slow.next.next;
+        return head;      
+    }
+}
+*/
