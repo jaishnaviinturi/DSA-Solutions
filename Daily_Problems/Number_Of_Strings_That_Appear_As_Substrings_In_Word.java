67f9e9721a1240a6312f5ad950423f6c2e5fa09a
jaishnaviinturi <jaishnaviinturi@gmail.com>
2026-06-29 21:02:03 +0530

    Added Daily Problem - 29/06/2026

---
+++ Daily_Problems/Number_Of_Strings_That_Appear_As_Substrings_In_Word.java
@@ -0,0 +1,13 @@
+//Link: https://leetcode.com/problems/number-of-strings-that-appear-as-substrings-in-word/
+
+class Solution {
+    public int numOfStrings(String[] patterns, String word) {
+        int count=0;
+        for(int i=0;i<patterns.length;i++){
+            if(word.contains(patterns[i])){
+                count++;
+            }
+        }
+        return count;
+    }
+}
